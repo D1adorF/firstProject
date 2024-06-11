3aaf11616aa3d837c34d3006bde38ca9ebd4aa1c
DiadorF <igorek-semeney@mail.ru>
2024-06-11 15:05:22 +0700

    Создание JWT Token в Laravel и его использование в Postman

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+    'namespace' => 'App\Http\Controllers',
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function () {
+    //dd($router);
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
+
+//Route::group(['namespace' => 'App\Http\Post'], function () {
+//    Route::get('/posts', 'IndexController');
+//});
+
+
+Route::group(['namespace' => 'App\Http\Controllers\Post'], function () {
+    Route::get('/posts', 'IndexController')->middleware('jwt.auth');
+});
